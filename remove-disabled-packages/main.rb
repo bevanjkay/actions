fed9c96f254530c908adf1a55ac88018d1f5f8e7
Bevan Kay <email@bevankay.me>
2024-06-17 13:12:32 +1000

    remove-disabled-packages: add action

--- remove-disabled-packages/main.rb
+++ remove-disabled-packages/main.rb
@@ -1,45 +1,64 @@
-require 'formula'
-require 'cask'
+# typed: true
+# frozen_string_literal: true
 
+# rubocop:disable Style/TopLevelMethodDefinition
 
-one_year_ago = Date.today << 12
+require "English"
+# typed: true
+require "formula"
+require "cask"
+
+ONE_YEAR_AGO = (Date.today << 12).freeze
+TARGET_TAP = ENV.fetch("GITHUB_REPOSITORY").freeze
 
 def git(*args)
-  system 'git', *args
-  exit $?.exitstatus unless $?.success?
+  system "git", *args
+  exit $CHILD_STATUS.exitstatus unless $CHILD_STATUS.success?
 end
 
 def find_disabled(packages: [])
   packages.select do |package|
+    next false if package.tap != TARGET_TAP.path
     next false unless package.disabled?
     next false if package.disable_date.nil?
 
-    package.disable_date < one_year_ago
+    puts "#{package.name} is disabled"
+
+    package.disable_date < ONE_YEAR_AGO
   end
 end
 
-puts 'Finding disabled packages...'
+def sourcefile_path(package)
+  package.is_a?(Formula) ? package.path : package.sourcefile_path
+end
 
-packages_to_remove = find_disabled(packages: Formula.installed + Cask::Caskroom.casks)
+puts "Finding disabled packages..."
 
-packages_to_remove.each { |package| FileUtils.rm package.path }
+packages_to_remove = find_disabled(packages: Formula.all + Cask::Cask.all)
 
-tap_dir = Tap.fetch(ENV.fetch("GITHUB_REPOSITORY")).path
+packages_to_remove.each { |package| FileUtils.rm sourcefile_path(package) }
 
-out, err, status = Open3.capture3 'git', '-C', tap_dir.to_s, 'status', '--porcelain', '--ignore-submodules=dirty'
+tap_dir = TARGET_TAP.path
+
+out, err, status = Open3.capture3 "git", "-C", tap_dir.to_s, "status", "--porcelain", "--ignore-submodules=dirty"
 raise err unless status.success?
 
 if out.chomp.empty?
-  puts 'No packages removed.'
-  File.open(ENV['GITHUB_OUTPUT'], 'a') { |f| f.puts('packages-removed=false') }
+  puts "No packages removed."
+  File.open(ENV.fetch("GITHUB_OUTPUT", nil), "a") { |f| f.puts("packages-removed=false") }
   exit
 end
 
-git '-C', tap_dir.to_s, 'add', '--all'
+puts "Removing disabled packages..."
+
+git "-C", tap_dir.to_s, "add", "--all"
 
 packages_to_remove.each do |package|
   puts "Removed `#{package.name}`."
-  git '-C', tap_dir.to_s, 'commit', package.path.to_s, '--message', "#{package.name}: remove #{package.is_a?(Formula) ? 'formula' : 'cask'}", '--quiet'
+  git "-C", tap_dir.to_s, "commit", sourcefile_path(package), "--message",
+      "#{package.name}: remove #{package.is_a?(Formula) ? "formula" : "cask"}", "--quiet"
 end
 
-File.open(ENV['GITHUB_OUTPUT'], 'a') { |f| f.puts('packages-removed=true') }
+File.open("./output.txt", "a") { |f| f.puts("packages-removed=true") }
+
+# rubocop:enable Style/TopLevelMethodDefinition
